2a13a14f982cdb2f08e27f1e4adcd3fa391e0b95
mohamed7sameer <mohammed7sameer2617@gmail.com>
2025-04-19 15:39:35 +0200

    aa

--- resources/views/livewire/qrcode/print-category.blade.php
+++ resources/views/livewire/qrcode/print-category.blade.php
@@ -20,19 +20,19 @@ class extends Component {
             width: 100%;
             height: 100%;
         }
-        .mo-svg-qucode svg rect{
+        /* .mo-svg-qucode svg rect{
             fill: transparent;
         }
         .mo-svg-qucode svg path{
             fill: white;
-        }
+        } */
     </style>
     @if(count($qrcodes))
         <div class="flex flex-wrap  gap-0 items-center justify-center">
             
             @foreach ($qrcodes as $qrcode)
                 {{-- <div class="w-[472px] h-[413px] relative"> --}}
-                <div class="w-[4cm] h-[3.5cm] relative">
+                <div class="w-[3.9cm] h-[3.5cm] relative">
                     <img src="{{ asset('assets/print_image.png') }}" class="w-full h-full">
                     <div class="mo-svg-qucode w-[2cm] h-[2cm] absolute left-[.2cm] top-[.4cm] bg-red">
                         {!! $qrcode->qrcode_svg !!}
